57f4ce8a7b8c4d2632980e20ce90f2a9e91614b3
VashuTheGreat <vanshsharma7832@gmail.com>
2026-09-26 18:14:55 +0530

    allow all types for test from api

--- api/schemas/chat_schema.py
+++ api/schemas/chat_schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Literal
+from src.core.constants import DEFAULT_DEFFICULTY_LEVEL,DEFAULT_EXAM_TYPE,DEFAULT_SUBJECT_NAME,DEFAULT_TOTAL_NO_OF_QUESTIONS
 
 class ChatRequest(BaseModel):
     message: str = Field(..., description="User message to send to the chat pipeline")
@@ -7,8 +8,8 @@ class ChatRequest(BaseModel):
 
 
 class TestGenerationRequest(BaseModel):
-    total_no_of_questions: int = Field(2, description="Number of questions to generate")
-    level: Literal['easy', 'medium', 'hard'] = Field('medium', description="Difficulty level of the questions")
-    subject_name: str = Field("maths", description="Subject for which questions should be generated")
-    exam_type: str = Field("IIT_JEE", description="Exam type the questions should match")
+    total_no_of_questions: int = Field(DEFAULT_TOTAL_NO_OF_QUESTIONS, description="Number of questions to generate")
+    level: str= Field(DEFAULT_DEFFICULTY_LEVEL, description="Difficulty level of the questions")
+    subject_name: str = Field(DEFAULT_SUBJECT_NAME, description="Subject for which questions should be generated")
+    exam_type: str = Field(DEFAULT_EXAM_TYPE, description="Exam type the questions should match")
                            
